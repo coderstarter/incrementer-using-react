be63faf9365fad8a87b2d887183602db4aaeb8c7
Abhishek Sharma <coder8248@gmail.com>
2023-02-04 15:48:53 +0530

    making the incrementer and decrementer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React from "react";
+// import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class based components
+
+// class HelloWorld extends React.Component {
+//   render() {
+//     return <h1>Hello world by</h1>;
+//   }
+// }
+
+// function based components
+
+// and passing the props for function based components
+
+// function HelloWorld(props) {
+//   console.log(props);
+//   // use destructuring
+//   const { name } = props;
+//   return <h1>Hello World by using function based components and props value is : {name}</h1>;
+// }
+
+
+// now will see for the class based how to pass the props
+
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 10
+    };
+  }
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="app">
+        <h2>{count}</h2>
+        <button
+          onClick={() => {
+            this.setState({ count: count + 1 })
+          }}>Increment</button>
+      </div>
+    );
+  }
 }
+export { HelloWorld };
 
-export default App;
+// this.props will have values
